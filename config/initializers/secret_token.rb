3289093166b85f4e4c872dbea8bf70955dd14710
dyep49 <dyep49@gmail.com>
2014-04-21 14:48:50 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularUmpireAuditor::Application.config.secret_key_base = '7adb1857c259b7b9907093ff34209b08ba9aefc7da39e5f7a45d02b943322586741775e6702d911aba3ccdb41997eba0469617b65dfbd67686344d6eb9a8ed31'
