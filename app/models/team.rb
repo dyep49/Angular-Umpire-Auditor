3adc6f3e8946cdfc0e1bc0e1a69ca54657564388
dyep49 <dyep49@gmail.com>
2014-07-19 14:45:54 -0400

    updated page title

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,11 +1,4 @@
 class Team < ActiveRecord::Base
 	has_and_belongs_to_many :games
 	has_many :pitchers
-
-
-	def set_title
-		full = self.full_name.split(' ')
-		team_city = self.city.split(' ')
-		(full - team_city).join('')
-	end
 end
