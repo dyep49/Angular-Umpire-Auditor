630217eede661cb103ef2ad17238ac2449801eaf
dyep49 <dyep49@gmail.com>
2015-04-04 00:27:59 -0400

    adds pitch spec

--- db/schema.rb
+++ db/schema.rb
@@ -18,21 +18,21 @@ ActiveRecord::Schema.define(version: 20150220194921) do
 
   create_table "days", force: :cascade do |t|
     t.datetime "game_date"
-    t.string   "umpire",                limit: 255
-    t.string   "home_team",             limit: 255
-    t.string   "away_team",             limit: 255
+    t.string   "umpire"
+    t.string   "home_team"
+    t.string   "away_team"
     t.float    "total_distance_missed"
     t.integer  "umpire_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "inning",                limit: 255
+    t.string   "inning"
     t.integer  "ball_count"
     t.integer  "strike_count"
     t.integer  "outs"
-    t.string   "inning_half",           limit: 255
+    t.string   "inning_half"
     t.integer  "pitch_id"
-    t.string   "play",                  limit: 255
-    t.string   "img_date",              limit: 255
+    t.string   "play"
+    t.string   "img_date"
   end
 
   create_table "favorites", force: :cascade do |t|
@@ -47,20 +47,20 @@ ActiveRecord::Schema.define(version: 20150220194921) do
   create_table "games", force: :cascade do |t|
     t.integer  "home_team_id"
     t.integer  "away_team_id"
-    t.string   "gid",              limit: 255
+    t.string   "gid"
     t.integer  "mlb_umpire_id"
     t.integer  "umpire_id"
     t.datetime "game_date"
-    t.integer  "total_calls",                  default: 0
-    t.integer  "correct_calls",                default: 0
-    t.integer  "incorrect_calls",              default: 0
-    t.string   "home_team_abbrev", limit: 255
-    t.string   "away_team_abbrev", limit: 255
-    t.float    "percent_correct",              default: 0.0
-    t.string   "umpire_name",      limit: 255
-    t.string   "game_type",        limit: 255
-    t.string   "home_full_name",   limit: 255
-    t.string   "away_full_name",   limit: 255
+    t.integer  "total_calls",      default: 0
+    t.integer  "correct_calls",    default: 0
+    t.integer  "incorrect_calls",  default: 0
+    t.string   "home_team_abbrev"
+    t.string   "away_team_abbrev"
+    t.float    "percent_correct",  default: 0.0
+    t.string   "umpire_name"
+    t.string   "game_type"
+    t.string   "home_full_name"
+    t.string   "away_full_name"
   end
 
   add_index "games", ["umpire_id"], name: "index_games_on_umpire_id", using: :btree
@@ -71,29 +71,29 @@ ActiveRecord::Schema.define(version: 20150220194921) do
   end
 
   create_table "pitchers", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "team",       limit: 255
+    t.string   "name"
+    t.string   "team"
     t.integer  "pid"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "pitches", force: :cascade do |t|
-    t.string   "gid",                   limit: 255
+    t.string   "gid"
     t.datetime "date_string"
-    t.string   "description",           limit: 255
+    t.string   "description"
     t.integer  "pid"
     t.float    "x_location"
     t.float    "y_location"
     t.float    "sz_top"
     t.float    "sz_bottom"
-    t.string   "sv_id",                 limit: 255
-    t.string   "type_id",               limit: 255
+    t.string   "sv_id"
+    t.string   "type_id"
     t.boolean  "missing_data"
     t.boolean  "correct_call"
-    t.float    "distance_missed_x",                 default: 0.0
-    t.float    "distance_missed_y",                 default: 0.0
-    t.float    "total_distance_missed",             default: 0.0
+    t.float    "distance_missed_x",     default: 0.0
+    t.float    "distance_missed_y",     default: 0.0
+    t.float    "total_distance_missed", default: 0.0
     t.integer  "pitcher_id"
     t.integer  "mlb_umpire_id"
     t.integer  "batter_id"
@@ -101,43 +101,43 @@ ActiveRecord::Schema.define(version: 20150220194921) do
     t.integer  "ball_count"
     t.integer  "strike_count"
     t.integer  "outs"
-    t.string   "inning_half",           limit: 255
+    t.string   "inning_half"
     t.integer  "inning"
     t.text     "play"
   end
 
   create_table "teams", force: :cascade do |t|
     t.integer  "team_id"
-    t.string   "abbreviation", limit: 255
-    t.string   "full_name",    limit: 255
+    t.string   "abbreviation"
+    t.string   "full_name"
     t.integer  "division_id"
     t.integer  "league_id"
-    t.string   "code",         limit: 255
-    t.string   "city",         limit: 255
-    t.string   "name_brief",   limit: 255
+    t.string   "code"
+    t.string   "city"
+    t.string   "name_brief"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "title",        limit: 255
+    t.string   "title"
   end
 
   create_table "umpires", force: :cascade do |t|
-    t.string  "name",          limit: 255
+    t.string  "name"
     t.integer "mlb_umpire_id"
   end
 
   add_index "umpires", ["mlb_umpire_id"], name: "my_index", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
---
+++ spec/models/pitch_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+
+describe Pitch do
+  describe "given a ball, called a strike" do
+    before do 
+      description = "Called Strike"
+      x_location = -0.21
+      y_location = 1.1
+      sz_top = 3.65
+      sz_bottom = 1.75
+      @pitch = Pitch.new(description: description, y_location: y_location, x_location: x_location, sz_top: sz_top, sz_bottom: sz_bottom)
+    end
+    describe "when asked if correct call" do
+      it "should take the pitch and" do
+        expect(@pitch.correct_call?).to eq(false)
+        expect(@pitch.y_miss).to eq(0.65)
+      end
+    end
+  end
+  describe "given a strike, called a strike" do 
+    before do
+      description = "Called Strike"
+      x_location = -0.21
+      y_location = 1.9
+      sz_top = 3.65
+      sz_bottom = 1.75
+      @pitch = Pitch.new({description: description, y_location: y_location, x_location: x_location, sz_top: sz_top, sz_bottom: sz_bottom})
+    end
+    describe "when asked if correct call" do
+      it "should take the pitch and" do
+        expect(@pitch.correct_call?).to eq(true)        
+      end
+    end
+  end
+  describe "given a ball, called a strike" do 
+    before do
+      description = "Called Strike"
+      x_location = -0.21
+      y_location = 1.1
+      sz_top = 3.65
+      sz_bottom = 1.75
+      @pitch = Pitch.new({description: description, y_location: y_location, x_location: x_location, sz_top: sz_top, sz_bottom: sz_bottom})
+    end
+    describe "when asked if correct call" do
+      it "should take the pitch and" do
+        expect(@pitch.correct_call?).to eq(false)
+      end
+    end
+  end
+  describe "given a ball, called a ball" do 
+    before do
+      description = "Ball"
+      x_location = -0.21
+      y_location = 1.1
+      sz_top = 3.65
+      sz_bottom = 1.75
+      @pitch = Pitch.new({description: description, y_location: y_location, x_location: x_location, sz_top: sz_top, sz_bottom: sz_bottom})
+    end
+    describe "when asked if correct call" do
+      it "should take the pitch and" do
+        expect(@pitch.correct_call?).to eq(true)
+      end
+    end
+  end
+end
