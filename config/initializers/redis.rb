9e10d0270e0e4adbd68af901b8efd42f03b1bcb4
dyep49 <dyep49@gmail.com>
2015-04-05 01:17:24 -0400

    still working on redis settings

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,7 @@
 
 # if ENV["RAILS_ENV"] == "production" 
-  uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/")
+  # uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/")
+  uri = URI.parse(ENV["REDISTOGO_URL"])
   $redis = Redis::Namespace.new("umpire_auditor", :host => uri.host, :port => uri.port, :password => uri.password)
 # else 
 #   $redis = Redis::Namespace.new("umpire_auditor", :redis => Redis.new)
