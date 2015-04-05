023110b217d35462420f387ecec1cdd265035b14
dyep49 <dyep49@gmail.com>
2015-04-05 01:19:45 -0400

    removed redis localhost reference

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,6 @@
 
 # if ENV["RAILS_ENV"] == "production" 
-  # uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/")
-  uri = URI.parse(ENV["REDISTOGO_URL"])
+  uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/")
   $redis = Redis::Namespace.new("umpire_auditor", :host => uri.host, :port => uri.port, :password => uri.password)
 # else 
 #   $redis = Redis::Namespace.new("umpire_auditor", :redis => Redis.new)
