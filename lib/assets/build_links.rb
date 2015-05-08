29acbd548ff11dadde037bfd6ce590ab1ac4f1e4
dyep49 <dyep49@gmail.com>
2015-05-07 23:56:30 -0400

    adds tests to build links module

--- lib/assets/build_links.rb
+++ lib/assets/build_links.rb
@@ -4,46 +4,33 @@ module BuildLinks
 
 	URL_ROOT = "http://gd2.mlb.com/components/game/mlb/year_#{ROOT_YEAR}"
 
-	def self.gid_info
-		gids = []
-		gid_url_array = []
-		(1..12).each do |month|
-			month = sprintf '%02d', month
-			full_month = "month_#{month}"
-			(1..31).each do |day|
-				day = sprintf '%02d', day
-				day_url = "#{URL_ROOT}/#{full_month}/day_#{day}"
-				gids = self.parse_gids(day_url)
-				gids.each do |gid|
-					gid_url_array << "#{day_url}/#{gid}/"
-				end
-			end
-		end
-		gid_url_array
-	end
+	def self.gid_info 
+		start_date = Date.new(ROOT_YEAR, 01, 01)
+		end_date = Date.new(ROOT_YEAR, 12, 31)
 
-	def self.parse_gids(day_url)
-		response = HTTParty.get("#{day_url}")
-		gids = response.scan(/gid[^\/]*/)
+		(start_date..end_date).map do |date|
+			self.parse_gids_by_date(date)
+		end
 	end
 
 	def self.parse_gids_by_date(date)
-		gid_url_array = []
 		day_url = self.date_to_gids_url(date)
 		gids = self.parse_gids(day_url)
-		gids.each do |gid|
-			gid_url_array << "#{day_url}/#{gid}"
-		end
-		gid_url_array
 	end
 
+	private
+	def self.parse_gids(day_url)
+		response = HTTParty.get("#{day_url}")
+		gids = response.scan(/gid[^\/]*/).uniq
+		gids.map { |gid| "#{day_url}/#{gid}" }
+	end
+
+	private
 	def self.date_to_gids_url(date)
 		month = sprintf '%02d', date.month
 		day = sprintf '%02d', date.day
 		day_url = "#{URL_ROOT}/month_#{month}/day_#{day}"
 	end
-
-
 
 end
 
